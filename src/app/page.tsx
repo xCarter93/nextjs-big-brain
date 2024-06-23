f851397b2d74032f4fa5892bf536ebcd384a31e5
Patrick Carter <xCarter93@gmail.com>
2024-06-23 11:42:22 -0400

    added dark mode toggle

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,6 +9,8 @@ import {
 } from "convex/react";
 import { createDocument } from "../../convex/documents";
 import { api } from "../../convex/_generated/api";
+import { Button } from "@/components/ui/button";
+import { ModeToggle } from "@/components/ui/theme-toggle";
 
 export default function Home() {
 	const createDocument = useMutation(api.documents.createDocument);
@@ -20,9 +22,10 @@ export default function Home() {
 			</Unauthenticated>
 			<Authenticated>
 				<UserButton />
-				<button onClick={() => createDocument({ title: "hello world" })}>
+				<ModeToggle />
+				<Button onClick={() => createDocument({ title: "hello world" })}>
 					Click Me
-				</button>
+				</Button>
 				{documents?.map((document) => (
 					<div key={document._id}>{document.title}</div>
 				))}
--- src/components/Providers.tsx
+++ src/components/Providers.tsx
@@ -3,6 +3,7 @@
 import { ClerkProvider, useAuth } from "@clerk/nextjs";
 import { ConvexReactClient } from "convex/react";
 import { ConvexProviderWithClerk } from "convex/react-clerk";
+import { ThemeProvider } from "./ThemeProvider";
 
 const convex = new ConvexReactClient(
 	process.env.NEXT_PUBLIC_CONVEX_URL as string
@@ -10,12 +11,19 @@ const convex = new ConvexReactClient(
 
 export default function Providers({ children }: { children: React.ReactNode }) {
 	return (
-		<ClerkProvider
-			publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY}
+		<ThemeProvider
+			attribute="class"
+			defaultTheme="dark"
+			enableSystem
+			disableTransitionOnChange
 		>
-			<ConvexProviderWithClerk client={convex} useAuth={useAuth}>
-				{children}
-			</ConvexProviderWithClerk>
-		</ClerkProvider>
+			<ClerkProvider
+				publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY}
+			>
+				<ConvexProviderWithClerk client={convex} useAuth={useAuth}>
+					{children}
+				</ConvexProviderWithClerk>
+			</ClerkProvider>
+		</ThemeProvider>
 	);
 }
