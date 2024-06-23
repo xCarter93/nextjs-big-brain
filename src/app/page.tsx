fdf0ffdfcbe17536d00daca6b2ca4267d219ae85
Patrick Carter <xCarter93@gmail.com>
2024-06-23 10:44:11 -0400

    initial convex sestup and hooking up front end to database

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,18 @@
 "use client";
 
 import { SignInButton, UserButton } from "@clerk/nextjs";
-import { Authenticated, Unauthenticated } from "convex/react";
+import {
+	Authenticated,
+	Unauthenticated,
+	useMutation,
+	useQuery,
+} from "convex/react";
+import { createDocument } from "../../convex/documents";
+import { api } from "../../convex/_generated/api";
 
 export default function Home() {
+	const createDocument = useMutation(api.documents.createDocument);
+	const documents = useQuery(api.documents.getDocuments);
 	return (
 		<main className="flex min-h-screen flex-col items-center justify-between p-24">
 			<Unauthenticated>
@@ -11,6 +20,12 @@ export default function Home() {
 			</Unauthenticated>
 			<Authenticated>
 				<UserButton />
+				<button onClick={() => createDocument({ title: "hello world" })}>
+					Click Me
+				</button>
+				{documents?.map((document) => (
+					<div key={document._id}>{document.title}</div>
+				))}
 			</Authenticated>
 		</main>
 	);
