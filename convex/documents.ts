a5a68a178a4f23ecf0c470928aa6bc0393a828f1
Patrick Carter <xCarter93@gmail.com>
2024-06-23 11:28:34 -0400

    scoping data to user identifier

--- convex/documents.ts
+++ convex/documents.ts
@@ -1,17 +1,27 @@
 import { mutation, query } from "./_generated/server";
-import { v } from "convex/values";
+import { ConvexError, v } from "convex/values";
 
 export const createDocument = mutation({
 	args: {
 		title: v.string(),
 	},
 	async handler(ctx, args) {
-		await ctx.db.insert("documents", { title: args.title });
+		const userId = (await ctx.auth.getUserIdentity())?.tokenIdentifier;
+		if (!userId) throw new ConvexError("Not authenticated");
+		await ctx.db.insert("documents", {
+			title: args.title,
+			tokenIdentifier: userId,
+		});
 	},
 });
 
 export const getDocuments = query({
 	async handler(ctx) {
-		return await ctx.db.query("documents").collect();
+		const userId = (await ctx.auth.getUserIdentity())?.tokenIdentifier;
+		if (!userId) return [];
+		return await ctx.db
+			.query("documents")
+			.withIndex("by_tokenIdentifier", (q) => q.eq("tokenIdentifier", userId))
+			.collect();
 	},
 });
